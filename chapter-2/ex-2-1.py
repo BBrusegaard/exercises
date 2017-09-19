10629e89b7beb0bf14b82a0fb125d780472a306a
Ben Brusegaard <ben-brusegaard@student.kirkwood.edu>
2017-09-19 13:42:35 +0000

    efactored to use variables

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -1,25 +1,20 @@
-# Programming Exercise 2-1
+#Programming Exercise 2-1
 # 
 # Program to display one person's contact information.
 # This program needs no input
 # and performs no computations.
 # It will display the name and contact information for a teacher.
 
-# display the full name
+full_name = "Ben Brusegaard"
+address = "1640 Moon Drive"
+city_state_zip = "Cupertino, Ca, 95014"
+phone = "(999)999-9999"
+course_area = "CIS-121"
 
 
-# display the address
-
-
-# display the city, state and zip
-
-
-# display the phone number
-
-
-# display the course area
-
-
-
-
+print(full_name)
+print(address)
+print(city_state_zip)
+print(phone)
+print(course_area)
 
